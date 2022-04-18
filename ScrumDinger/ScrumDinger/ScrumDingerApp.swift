992f9450e2866170bdcfa3d827928b905fd0e25c
Trevor Keegan <positev@outlook.com>
2022-04-18 11:59:29 -0500

    Add initial project

---
+++ ScrumDinger/ScrumDinger/ScrumDingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerApp.swift
+//  ScrumDinger
+//
+//  Created by Trevor Keegan on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
